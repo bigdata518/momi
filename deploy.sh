4bae589f9f4da03e7df9e1d76e65bcff02dc521e
jianying9 <jianying9@jianying9>
2014-05-23 17:26:26 +0800

    1、删除自动部署脚本，自动部署功能采用hudson脚本实现

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-/mnt/raid/glassfish3/bin/asadmin stop-domain domain2
-rm -rf /mnt/raid/glassfish3/glassfish/domains/domain2/autodeploy/*
-cp ./target/*.war /mnt/raid/glassfish3/glassfish/domains/domain2/autodeploy/
-/mnt/raid/glassfish3/bin/asadmin start-domain domain2
