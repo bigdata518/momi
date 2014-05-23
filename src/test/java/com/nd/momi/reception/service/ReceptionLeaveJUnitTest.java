b1e41816f0c1d047180cef47596c89b98bb8a073
卢桂芳 <luguif@nd.com.cn>
2014-05-23 11:51:08 +0800

    客服离开，回岗接口测试

---
+++ src/test/java/com/nd/momi/reception/service/ReceptionLeaveJUnitTest.java
@@ -0,0 +1,43 @@
+package com.nd.momi.reception.service;
+
+import com.nd.momi.AbstractMomiTest;
+import com.nd.momi.config.ActionNames;
+import com.wolf.framework.config.DefaultResponseFlags;
+import com.wolf.framework.worker.context.Response;
+import junit.framework.Assert;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author aladdin
+ */
+public class ReceptionLeaveJUnitTest extends AbstractMomiTest {
+
+    public ReceptionLeaveJUnitTest() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+    //
+
+    @Test
+    public void test() {
+        this.setReceptionSession("271411");
+        Map<String, String> parameterMap = new HashMap<String, String>(2, 1);
+        parameterMap.put("receptionId", "271411");
+        parameterMap.put("offMessage", "吃饭");
+        Response response = this.testHandler.execute(ActionNames.RECEPTION_LEAVE, parameterMap);
+        System.out.println(response.getResponseMessage());
+        Assert.assertEquals(response.getFlag(), DefaultResponseFlags.SUCCESS);
+    }
+}
