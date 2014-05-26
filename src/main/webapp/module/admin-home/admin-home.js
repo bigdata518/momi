e32f7ee9bda4041259a42d03d37d1bcb4430a590
jianying9 <jianying9@jianying9>
2014-05-26 17:41:15 +0800

    1、增加管理-公告管理功能

--- src/main/webapp/module/admin-home/admin-home.js
+++ src/main/webapp/module/admin-home/admin-home.js
@@ -30,71 +30,21 @@ define(function(require) {
                     thisCom.selected();
                     //
                     var panelItem = panelList.getItemByKey(thisCom.key);
-                    if(!panelItem) {
+                    if (!panelItem) {
                         panelItem = panelList.addItemData({moduleId: thisCom.key});
                     }
                     var zIndex = _index.nextZIndex();
                     panelItem.$this.css({zIndex: zIndex});
-                    _module.loadModule(itemCom.key, function() {}, panelItem);
+                    _module.loadModule(itemCom.key, function() {
+                    }, panelItem);
                 });
             }
         });
         navigationList.loadData([
-            {navigation: '客服帐号', moduleId: 'reception-manage'}
+            {navigation: '客服帐号', moduleId: 'reception-manage'},
+            {navigation: '公告管理', moduleId: 'bulletin-manage'}
         ]);
         navigationList.firstChild.$this.click();
-        //
-        //默认加载userPanel
-//        var receptionManagePanel = thisModule.findByKey('reception-manage-panel');
-//        var toReceptionManageButton = thisModule.findByKey('to-reception-manage-button');
-        //导航集合
-//        var navs = [
-//            {
-//                panel: receptionManagePanel,
-//                btn: toReceptionManageButton,
-//                module: 'reception-manage'
-//            }
-//        ];
-        //默认页面
-//        module.loadModule(receptionManagePanel.id, 'reception-manage', function() {
-//        });
-//        thisModule.setContext({
-//            visiblePanel: receptionManagePanel,
-//            visibleButton: toReceptionManageButton
-//        });
-        //绑定导航按钮事件
-//        var panel;
-//        var btn;
-//        for (var index = 0; index < navs.length; index++) {
-//            btn = navs[index].btn;
-//            _event.bind(btn, 'click', function(thisCom) {
-//                var visiblePanel = thisModule.getContext('visiblePanel');
-//                var visibleButton = thisModule.getContext('visibleButton');
-//                var thisPanel;
-//                var thisBtn;
-//                var moduleName;
-//                if (thisCom !== visibleButton) {
-//                    visiblePanel.hide();
-//                    visibleButton.$this.removeClass('selected');
-//                    for (var index = 0; index < navs.length; index++) {
-//                        thisPanel = navs[index].panel;
-//                        thisBtn = navs[index].btn;
-//                        moduleName = navs[index].module;
-//                        if (thisBtn === thisCom) {
-//                            thisPanel.show();
-//                            thisBtn.$this.addClass('selected');
-//                            thisModule.setContext({
-//                                visiblePanel: thisPanel,
-//                                visibleButton: thisBtn
-//                            });
-//                            module.loadModule(thisPanel.id, moduleName, function() {
-//                            });
-//                            break;
-//                        }
-//                    }
-//                }
-//            });
-//        }
     };
     return self;
 });
