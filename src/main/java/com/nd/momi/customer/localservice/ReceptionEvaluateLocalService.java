52d90b0e2bae053a24bcdc05ddef50e222755050
晁阳 <cy150818@nd.com.cn>
2014-05-27 18:22:55 +0800

    修改玩家评价客服功能代码

--- src/main/java/com/nd/momi/customer/localservice/ReceptionEvaluateLocalService.java
+++ src/main/java/com/nd/momi/customer/localservice/ReceptionEvaluateLocalService.java
@@ -5,14 +5,10 @@ import com.nd.momi.customer.entity.ReceptionEvaluateEntity;
 import com.wolf.framework.local.Local;
 
 import java.util.List;
-
-/**
- *
- * @author cy
- */
+import java.util.Map;
 
 public interface ReceptionEvaluateLocalService extends Local{
-    //客服评价无返回参数
+
     public void insertReceptionEvaluate(String customerId, String receptionId, int score, String receptionQuality, String problemSolve, String suggestion);
 
     public void deleteReceptionEvaluate(String evaluateId);
